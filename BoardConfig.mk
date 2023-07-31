ca38c0e6d70a3479826105f0cedffa108562ce36
CodeChas3r1 <codechas3r@gmail.com>
2023-07-31 12:18:04 +0800

    rosemary: Switch to r450784d-clang
    Signed-off-by: CodeChas3r1 <codechas3r@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/redmi/rosemary
 TARGET_KERNEL_CONFIG := rosemary_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r450784d
 TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 LLVM_IAS=1 AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
